757ba37c131c7d26f06e835d5d1d45feb45550d9
HYDGITHHUB <2607223301@qq.com>
2020-04-03 20:35:59 +0800

    build:build remoteconfig

---
+++ remoteconfigclient/src/main/java/com/study/controller/RemoteConfigHandler.java
@@ -0,0 +1,20 @@
+package com.study.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remote")
+public class RemoteConfigHandler {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/getPort")
+    public String getPort() {
+        return this.port;
+    }
+
+}
